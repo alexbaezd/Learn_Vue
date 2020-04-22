9099df86891849562e4d7646cb99c5f4fe9e9af2
Alejandro Báez <baezdalex@gmail.com>
2020-04-21 20:00:24 -0500

    🤟 Add First Component => Hello
    Hello World, simple animation.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
-    <h1>Hello Learn Vue</h1>
+    <Hello msg="Hello Vue"/>
     <p>I know React.</p>
   </div>
 </template>
 
 <script>
+import Hello from "./components/Hello.vue";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    Hello
+  }
 };
 </script>
 
@@ -22,5 +25,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#app img {
+  animation: heart 1500ms infinite;
+}
+@keyframes heart {
+  0% {
+    transform: scale(1.1);
+  }
+  50% {
+    transform: scale(1);
+  }
+
+  100% {
+    transform: scale(1.1);
+  }
 }
 </style>
