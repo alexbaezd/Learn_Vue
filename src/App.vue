5101e8993d14e91e4012e7efa23fd643038abed0
=Alejandro Baez <baezdalex@gmail.com>
2020-06-02 09:53:25 -0500

    🔥 Vue Project with Vue CLI.   Products  from the components branch

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,156 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <Hello msg="Hello Vue"/>
-    <p>I know React.</p>
+    <button @click="toggleShow">
+      <span v-if="isShowing">Hide products</span>
+      <span v-else>Show Products</span>
+    </button>
+    <div class="products" v-if="isShowing">
+      <div class="unit" v-for="(unit, i) in manifest" :key="i">
+        <child :item="unit.item" :url="unit.url"></child>
+      </div>
+    </div>
+    <br />
   </div>
 </template>
 
 <script>
-import Hello from "./components/Hello.vue";
-
+import Child from "@/components/Child.vue";
 export default {
   name: "App",
   components: {
-    Hello
+    Child
+  },
+  data() {
+    return {
+      manifest: [
+        {
+          item: "backpack",
+          url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/backpack.jpg"
+        },
+        {
+          item: "tshirt",
+          url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/tshirt.jpg"
+        },
+        {
+          item: "sweatshirt",
+          url:
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/sweatshirt.jpg"
+        }
+      ],
+      isShowing: true
+    };
+  },
+  methods: {
+    toggleShow() {
+      this.isShowing = !this.isShowing;
+    }
   }
 };
 </script>
 
 <style>
+body,
+html {
+  font-family: "PT Serif", serif;
+}
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.products {
+  display: -webkit-box;
+  display: flex;
+  flex-wrap: wrap;
+  -webkit-box-pack: center;
+  justify-content: center;
+}
+
+.special,
+h1,
+h2,
+h3,
+li,
+button.submit,
+.quant-text {
+  font-family: "Montserrat", sans-serif;
+  text-transform: uppercase;
+  letter-spacing: 0.08em;
+  font-size: 12px;
+}
+
+h2 {
+  font-size: 20px;
+  margin: 0 0 5px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
-#app img {
-  animation: heart 1500ms infinite;
+li {
+  list-style: none;
+  display: inline-block;
 }
-@keyframes heart {
-  0% {
-    transform: scale(1.1);
-  }
-  50% {
-    transform: scale(1);
-  }
 
-  100% {
-    transform: scale(1.1);
-  }
+input {
+  background: #eee;
+}
+
+button {
+  border: none;
+  border-radius: 1000px;
+  padding: 8px 13px;
+  outline: none;
+  cursor: pointer;
+}
+button.inc {
+  background: white;
+  color: black;
+}
+button.submit {
+  background: black;
+  color: white;
+  margin-left: 20px;
+  padding: 12px 14px;
+  width: 240px;
+  -webkit-transition: 0.25s all ease-in;
+  transition: 0.25s all ease-in;
+}
+button.submit:hover {
+  opacity: 0.8;
+  -webkit-transition: 0.25s all ease-out;
+  transition: 0.25s all ease-out;
+}
+
+.quant-text {
+  display: inline-block;
+  text-align: center;
+  width: 140px;
+  padding: 8px;
+  padding: 8px;
+}
+
+.quantity {
+  border: 2px solid black;
+  width: 235px;
+  padding-left: 3px;
+  border-radius: 1000px;
+  background: #fff;
+  margin-left: 20px;
+}
+
+.item {
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+  width: 280px;
+  height: 440px;
+  justify-content: space-around;
+  background: #eee;
+  padding: 20px 10px;
+  margin: 30px;
+}
+.item img {
+  align-self: center;
 }
 </style>
